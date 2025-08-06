d051b6d7f069b059365248d996cdf9404cde9896
Ayush Maheshwari <111050424+FrostScythe@users.noreply.github.com>
2025-08-06 21:03:36 +0530

    Update WaterVehicle.java

--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -1,65 +1,6 @@
 package com.driver;
 
-// Subtask 1: Class Hierarchy - Vehicle -> Car -> F1
-
-class Vehicle {
-    void start() {
-        System.out.println("Vehicle started");
-    }
-}
-
-class Car extends Vehicle {
-    void drive() {
-        System.out.println("Car is driving");
-    }
-}
-
-class F1 extends Car {
-    void race() {
-        System.out.println("F1 car is racing");
-    }
-}
-
-// Subtask 2: WaterVehicle interface and Boat class
-
-interface WaterVehicle {
-    String getVehicleName();
-    int getVehicleCapacity();
-}
-
-class Boat implements WaterVehicle {
-    String name;
-    int capacity;
-
-    // Constructor
-    public Boat(String name, int capacity) {
-        this.name = name;
-        this.capacity = capacity;
-    }
-
-    // Implementing interface methods
-    public String getVehicleName() {
-        return name;
-    }
-
-    public int getVehicleCapacity() {
-        return capacity;
-    }
-}
-
-// Main driver code
-public class Main {
-    public static void main(String[] args) {
-
-        // Subtask 1 Testing
-        F1 myF1 = new F1();
-        myF1.start();   // from Vehicle
-        myF1.drive();   // from Car
-        myF1.race();    // from F1
-
-        // Subtask 2 Testing
-        Boat myBoat = new Boat("Sea Explorer", 15);
-        System.out.println("Boat name: " + myBoat.getVehicleName());
-        System.out.println("Boat capacity: " + myBoat.getVehicleCapacity());
-    }
+public interface WaterVehicle {
+    public String getVehicleName();
+    public int getVehicleCapacity();
 }
